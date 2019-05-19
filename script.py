411a665678000b60acfa4a7561518511b367f64e
iwut <anddra@kth.se>
2019-05-19 14:18:52 +0200

    update script

--- script.py
+++ script.py
@@ -1,28 +1,41 @@
 from socketIO_client_nexus import SocketIO, LoggingNamespace
+import logging
+
+logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
+logging.basicConfig()
+
 
 SERVER_IP = 'http://192.168.0.29'
 SERVER_PORT = 3001
 
+
 def on_connect():
     print('connect')
+
 
 def on_disconnect():
     print('disconnect')
 
+
 def on_reconnect():
     print('reconnect')
 
-def on_command(*args):
-    print('command', args)
+
+def on_command(data):
+    _type = data['type']
+
+    if _type == 'kill':
+        print('KILL')
+    else:
+        print('OTHER COMMAND')
+
 
 socket = SocketIO(SERVER_IP, SERVER_PORT)
 socket.on('connect', on_connect)
 socket.on('disconnect', on_disconnect)
 socket.on('reconnect', on_reconnect)
+socket.on('command', on_command)
 
 socket.emit('connect_drone')
 
-socket.on('command', on_command)
-
-while True:
-  hej = 1
+socket.wait()
