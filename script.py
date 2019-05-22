ad52517f15768dbda734b7b7a11f8c36e1049ad5
iwut <anddra@kth.se>
2019-05-22 22:03:56 +0200

    test stuff

--- script.py
+++ script.py
@@ -1,11 +1,26 @@
 from socketIO_client_nexus import SocketIO, LoggingNamespace
 import logging
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+PIN_TRIGGER = 15
+PIN_ECHO = 14
+
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+GPIO.output(PIN_TRIGGER, False)
+print('Waiting for Sensor to settle')
+time.sleep(2)
 
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
 
 
 SERVER_IP = 'http://192.168.0.29'
+# SERVER_IP = 'http://192.168.1.128'
 SERVER_PORT = 3001
 
 
@@ -28,6 +43,29 @@ def on_command(data):
         print('KILL')
     else:
         print('OTHER COMMAND')
+
+def get_measurement():
+    GPIO.output(PIN_TRIGGER, True)
+
+    time.sleep(0.00001)
+
+    GPIO.output(PIN_TRIGGER, False)
+
+    while GPIO.input(PIN_ECHO) == 0:
+        pulse_start = time.time()
+    
+    while GPIO.input(PIN_ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = round(17150 * pulse_duration, 2)
+
+    print('Distance is:', distance)
+
+    GPIO.cleanup()
+
+
 
 
 socket = SocketIO(SERVER_IP, SERVER_PORT)
